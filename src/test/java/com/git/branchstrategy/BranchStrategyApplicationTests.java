ea1541a53d617468a2196a15cb05ad42a59c6abb
Seongsoo <skysoo1111@naver.com>
2021-05-07 18:00:17 +0900

    Init Commit

---
+++ src/test/java/com/git/branchstrategy/BranchStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.branchstrategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchStrategyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
